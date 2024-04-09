6ff2350e9a6893da67c74bbab1b079097856562b
EstevaoSillva <estevaovaldivinosilva@gmail.com>
2024-04-09 00:17:48 -0400

    Altura e Idade

---
+++ Atividade_02/alturaIdade.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    float altura;
+    int idade;
+
+    cout << "Digite sua Altura: " << endl;
+    cin >> altura;
+    cout << "Digite sua Idade: " << endl;
+    cin >> idade;
+
+    cout << "Altura: " << altura << endl;
+    cout << "Idade: " << idade << endl;
+    return 0;
+
+}
